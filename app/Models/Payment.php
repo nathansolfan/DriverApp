b99ed1b111d4d54fd2ee3c40edb46ca563ee0787
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-11-06 15:48:23 +0000

    updates

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+    use HasFactory;
+    // relationship: a payment belongs to a booking
+    public function booking()
+    {
+        return $this->belongsTo(Booking::class);
+    }
 }
