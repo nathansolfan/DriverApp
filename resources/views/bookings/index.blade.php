490d3b40a8aaefc6df724a04c7ea914f54247265
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-11-07 16:19:41 +0000

    booking > index

---
+++ resources/views/bookings/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bookings</title>
+</head>
+<body>
+    <h1>Bookings</h1>
+    <a href=" {{route('bookings.create')}} ">Create Booking</a>
+    <ul>
+        @foreach ($bookings as $booking )
+        <li>User: {{$booking->user->name}}, Route: {{$booking->route->pickup_location}} to {{$booking->route->dropoff_location}}
+            <a href=" {{route('bookings.edit', $booking->id)}} ">Edit</a>
+            <form action=" {{route('bookings.destroy', $booking->id)}}" method="POST">
+                @csrf
+                @method('DELETE')
+            </form>
+        </li>
+        @endforeach
+    </ul>
+</body>
+</html>
