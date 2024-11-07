71b10ac70a18140e874e789e79275de6ff6145fa
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-11-07 16:10:00 +0000

    update

---
+++ resources/views/routes/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Routes</title>
+</head>
+<body>
+    <h1>Routes</h1>
+    <a href=" {{route('routes.create')}} ">Create a new Route</a>
+
+</body>
+</html>
