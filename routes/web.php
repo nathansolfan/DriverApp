a4b5591f14d47b4ed62241cc8a9db27b5580fe30
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-11-07 20:00:12 +0000

    update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\RouteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
+
+// Define resource routes for routes, bookings, and payments
+Route::resource('routes', RouteController::class);
+Route::resource('bookings', BookingController::class);
+Route::resource('payments', PaymentController::class);
