57b8ebc66b14460c7c83734b18f400cb39d01dc5
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-11-07 16:15:21 +0000

    routes > index

--- resources/views/routes/index.blade.php
+++ resources/views/routes/index.blade.php
@@ -9,6 +9,18 @@
 <body>
     <h1>Routes</h1>
     <a href=" {{route('routes.create')}} ">Create a new Route</a>
+    <ul>
+        @foreach ($routes as $route )
+        <li>{{ $route->pickup_location}} to {{ $route->dropoff_location}}
+            <a href=" {{route('routes.edit', $route->id)}} ">Edit</a>
+            <form action=" {{ route('routes.destroy', $route->id)}}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button type="submit">Delete</button>
+            </form>
+        </li>
+        @endforeach
+    </ul>
 
 </body>
 </html>
