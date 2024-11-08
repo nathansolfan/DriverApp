2ff8d0f55c3ff1dd525b73149dfd4b634179077f
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-11-08 16:24:58 +0000

    added onsubmit to confirm() if user wanna delete

--- resources/views/routes/index.blade.php
+++ resources/views/routes/index.blade.php
@@ -22,7 +22,7 @@
         @foreach ($routes as $route )
         <li>{{ $route->pickup_location}} to {{ $route->dropoff_location}}
             <a href=" {{route('routes.edit', $route->id)}} ">Edit</a>
-            <form action=" {{ route('routes.destroy', $route->id)}}" method="POST">
+            <form action=" {{ route('routes.destroy', $route->id)}}" method="POST" onsubmit="return confirm('Are you sure?');">
                 @csrf
                 @method('DELETE')
                 <button type="submit">Delete</button>
