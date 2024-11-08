fe6b7f861b37f9d342f0839d14d3a7fa430774d4
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-11-08 23:59:08 +0000

    updates

--- resources/views/routes/create.blade.php
+++ resources/views/routes/create.blade.php
@@ -7,11 +7,15 @@
 </head>
 <body>
     <h1>Create a New Route</h1>
+    <a href=" {{route('bookings.index')}} ">Go to Bookings</a>
     <form action="{{ route('routes.store') }}" method="POST">
         @csrf
         <div>
             <label for="pickup_location">Pickup Location:</label>
             <input type="text" id="pickup_location" name="pickup_location" required>
+            @error('pickup_location')
+            <span style="color: red;"> {{$message}} </span>
+            @enderror
         </div>
         <div>
             <label for="dropoff_location">Dropoff Location:</label>
