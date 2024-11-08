7a1ab91368b96bdbf58166f7681b122d63b35517
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-11-08 16:21:14 +0000

    flash msg when creating route

--- resources/views/routes/index.blade.php
+++ resources/views/routes/index.blade.php
@@ -8,6 +8,15 @@
 </head>
 <body>
     <h1>Routes</h1>
+    @if (session('success'))
+    <p style="color:green"> {{session('success')}} </p>
+    @endif
+    @if (session('error'))
+    <p style="color:red"> {{session('error')}} </p>
+
+    @endif
+
+
     <a href=" {{route('routes.create')}} ">Create a new Route</a>
     <ul>
         @foreach ($routes as $route )
