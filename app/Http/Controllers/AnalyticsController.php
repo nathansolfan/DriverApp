68a1f166a5f43a0133b37d6463c3a1839caff82b
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-11-09 11:56:20 +0000

    Updates

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Payment;
+use App\Models\Route;
 use Illuminate\Http\Request;
 
 class AnalyticsController extends Controller
 {
-    //
+    public function index()
+    {
+        // Get total number of routes - count() built in php function
+        $totalRoutes = Route::count();
+
+        // Get total number of bookings
+        $bookingsPerRoute = Route::withCount('bookings')->get();
+
+        // get total paymentss
+        $totalPayments = Payment::sum('amount');
+        return view('analytics.index', compact('totalRoutes', 'bookingsPerRoute'));
+    }
 }
