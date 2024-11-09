89bdc612d29ce34772cf938563405b38b05dce79
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-11-09 12:06:54 +0000

    updates

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -8,6 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'booking_id',
+        'amount',
+        'status',
+        'payment_date',
+    ];
+
+
+
+
     // relationship: a payment belongs to a booking
     public function booking()
     {
--- database/migrations/2024_11_06_151719_create_payments_table.php
+++ database/migrations/2024_11_06_151719_create_payments_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('booking_id')->constrained()->onDelete('cascade'); // Reference to the booking
+            $table->decimal('amount', 8,2)->default(0); // Amount for the payment
+            $table->string('status')->default('pending');
+            $table->date('payment_date')->nullable();
             $table->timestamps();
         });
     }
