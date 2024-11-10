498ce71f7461f974657b10791b1346e596dffcea
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-11-10 21:09:08 +0000

    update seeders

--- database/seeders/RouteSeeder.php
+++ database/seeders/RouteSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Route;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,7 @@ class RouteSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Route::factory()->count(10)->create();
+
     }
 }
