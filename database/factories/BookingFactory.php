464f346de1bcf658ddae0f30d00da3b395052687
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-11-11 16:07:54 +0000

    BookingFactory done

--- database/factories/BookingFactory.php
+++ database/factories/BookingFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Route;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,10 @@ class BookingFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::inRandomOrder()->first()->id,
+            'route_id' => Route::inRandomOrder()->first()->id,
+            'status' => $this->faker->randomElement(['Pending', 'Confirmed', 'Cancelled']),
+            'seat_count' => $this->faker->numberBetween(1,4),
         ];
     }
 }
