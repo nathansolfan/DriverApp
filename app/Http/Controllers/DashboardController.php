9e10d16fcc32c7cb36cd88cdd6549f1e880afd67
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-11-12 16:25:29 +0000

    update DashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Booking;
+use App\Models\Payment;
+use App\Models\Route;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+    public function index()
+    {
+        $totalRoutes = Route::count();
+        $totalBookings = Booking::count();
+        $totalPayments = Payment::sum('amount');
+
+        return view('dashboard', compact('totalRoutes', 'totalBookings', 'totalPayments'));
+    }
 }
