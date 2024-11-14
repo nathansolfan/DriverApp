5bf0bbf85540c1519fbd02b89e6bd1dd3b434d34
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-11-14 15:34:05 +0000

    inserted breadcrumbs

---
+++ routes/breadcrumbs.php
@@ -0,0 +1,33 @@
+<?php
+
+use Diglactic\Breadcrumbs\Breadcrumbs;
+use Diglactic\Breadcrumbs\Generator as BreadcrumbTrail;
+
+// Dashboard
+Breadcrumbs::for('dashboard', function (BreadcrumbTrail $trail) {
+    $trail->push('Dashboard', route('dashboard'));
+});
+
+// Routes Index
+Breadcrumbs::for('routes.index', function (BreadcrumbTrail $trail) {
+    $trail->parent('dashboard');
+    $trail->push('Routes', route('routes.index'));
+});
+
+// Bookings Index
+Breadcrumbs::for('bookings.index', function (BreadcrumbTrail $trail) {
+    $trail->parent('dashboard');
+    $trail->push('Bookings', route('bookings.index'));
+});
+
+// Payments Index
+Breadcrumbs::for('payments.index', function (BreadcrumbTrail $trail) {
+    $trail->parent('dashboard');
+    $trail->push('Payments', route('payments.index'));
+});
+
+// Booking Edit
+Breadcrumbs::for('bookings.edit', function (BreadcrumbTrail $trail, $booking) {
+    $trail->parent('bookings.index');
+    $trail->push('Edit Booking', route('bookings.edit', $booking));
+});
