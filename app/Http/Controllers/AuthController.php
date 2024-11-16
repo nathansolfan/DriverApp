ad694b71f7eea69ab6302922ba789b9eca960456
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-11-16 16:30:10 +0000

    updates

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -18,5 +20,15 @@ class AuthController extends Controller
             'email' => 'required|string|max:255',
             'password' => 'required|string|min:8|confirmed',
         ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        auth()->login($user);
+
+        return redirect()->route('dashboard')->with('success', 'User created');
     }
 }
