13cef54acf97c20e8cf19577b97974cd715d7fc9
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-11-16 16:27:39 +0000

    added into the AuthController the showRegistrationForm view and the register Request $request->validate

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function showRegistrationForm()
+    {
+        return view('auth.register');
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|max:255',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+    }
 }
