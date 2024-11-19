850e70c39aae50f8dd8758a6b66a94dd87f4b838
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-11-19 20:05:10 +0000

    updates

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -26,7 +26,9 @@ class AuthController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => Hash::make($request->password)
+            'password' => Hash::make($request->password),
+            // add role - default Customer
+            'role' => 'customer'
         ]);
 
         Auth::login($user);
