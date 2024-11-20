7ffa5749af0ccf470ae0943e0819e49e5d3fc870
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-11-20 16:28:59 +0000

    BookingController updated

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -7,7 +7,6 @@ use App\Models\Route;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Validation\UnauthorizedException;
 
 class BookingController extends Controller
 {
