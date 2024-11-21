494502dbc9b5117fe263dc766fad04b473705f32
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-11-21 19:23:15 +0000

    update logout from get to post

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,7 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 // Customer-specific routes
 Route::get('/customer/bookings', [BookingController::class, 'customerBookings'])->name('customer.bookings');
